1441b27550c8161e5d5144006b9d053781004886
Elijah <elijahsharper1@gmail.com>
2018-02-27 00:38:11 +0300

    async data with axios

---
+++ pages/posts/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>{{ posts }}</p>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        posts: []
+      }
+    },
+    // mounted () {
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //       .then((response) => {
+    //            this.posts = response.data
+    //        });
+    // }
+
+    async asyncData () {
+      // return axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      //   return {
+      //     posts: response.data
+      //   }
+      // })
+
+      let response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+      return {
+        posts: response.data
+      }
+    }
+
+  }
+</script>
